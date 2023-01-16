ea2c7245c8fb2f0b418233825a7d01ff2621c436
Dominic Talbot <dom.talbot@v12finance.com>
2023-01-16 15:49:58 +0000

    effort for android

--- android/src/main/java/com/reactlibrary/ReactNativeDeviceRiskModule.java
+++ android/src/main/java/com/reactlibrary/ReactNativeDeviceRiskModule.java
@@ -7,6 +7,9 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 
+import com.iovation.mobile.android.FraudForceConfiguration;
+import com.iovation.mobile.android.FraudForceManager;
+
 public class ReactNativeDeviceRiskModule extends ReactContextBaseJavaModule {
 
     private final ReactApplicationContext reactContext;
@@ -25,5 +28,17 @@ public class ReactNativeDeviceRiskModule extends ReactContextBaseJavaModule {
     public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
         // TODO: Implement some actually useful functionality
         callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
+    }
+
+    @ReactMethod
+    public void blackbox(final Promise promise) {
+        FraudForceConfiguration fraudForceConfiguration = new FraudForceConfiguration.Builder()
+            .build();
+
+        FraudForceManager fraudForceManager = FraudForceManager.getInstance();
+        fraudForceManager.initialize(fraudForceConfiguration, getReactApplicationContext());
+
+        String blackbox = fraudForceManager.getInstance().getBlackbox(getReactApplicationContext());
+        promise.resolve(blackbox);
     }
 }
